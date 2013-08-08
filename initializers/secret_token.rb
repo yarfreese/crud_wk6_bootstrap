d2e36213f623869f8ae40ea185aa4ca285c3438b
yarfreese <yarfreese@gmail.com>
2013-08-08 16:01:13 -0700

    bootstrap and timezone practice

---
+++ initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrudForWk6::Application.config.secret_key_base = '9ea5259042fab4e448d4287046edf9c68d836ada369e6b4b49d1a6cc5239e25e827ddcda46a096f2d1e1375b31b31bd2c6cbfb57d20a0199c3ef54eda77e0cd4'
